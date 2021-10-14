0a47aa5dda8b53bfe26456836dd0a33feebfef7c
Sam Wang <smwn@hey.com>
2021-10-13 18:51:29 -0700

    Created function

---
+++ SilicanDate/src/SilicanDate/Function.fs
@@ -0,0 +1,97 @@
+namespace SilicanDate
+
+
+open Amazon.Lambda.Core
+
+open System
+
+
+// Assembly attribute to enable the Lambda function's JSON input to be converted into a .NET class.
+[<assembly: LambdaSerializer(typeof<Amazon.Lambda.Serialization.SystemTextJson.DefaultLambdaJsonSerializer>)>]
+()
+
+
+type Function() =
+   static let syncPoint = DateTime(2018, 1, 1)
+
+   static let syncPointDayNumber =
+      12017 * 364
+      + 7 * ((12017 / 5) - (12017 / 40) + (12017 / 400))
+
+   static let daysIn400Years = 400 * 364 + 7 * (400 / 5 - 400 / 40 + 1)
+   static let daysIn40Years = 40 * 364 + 7 * (40 / 5 - 1)
+   static let daysIn5Years = 5 * 364 + 7
+
+   static let seasons =
+      [ "Nevari"
+        "Penari"
+        "Sevari"
+        "Venari" ]
+   
+   static let weeks =
+      [ "Ateluna"
+        "Beviruto"
+        "Deruna"
+        "Elito"
+        "Feridina"
+        "Geranito"
+        "Lunamarina"
+        "Miraliluto"
+        "Peridina"
+        "Samerito"
+        "Timina"
+        "Verato"
+        "Wilaluna"
+        "Zeroto" ]
+      
+   static let daysOfWeek =
+      [ "Boromika"
+        "Ferimanika"
+        "Lusinika"
+        "Navimilika"
+        "Perinatika"
+        "Relikanika"
+        "Temiranika" ]
+
+   /// <summary>
+   /// A simple function that takes a string and does a ToUpper
+   /// </summary>
+   /// <param name="input">A date in the format YYYY-MM-DD</param>
+   /// <returns>A string representation of the converted Silican date</returns>
+   member _.FunctionHandler(input: string) =
+      let parsedDate = DateTime.Parse(input)
+      let difference = (parsedDate - syncPoint).Days
+      let dayNumber = syncPointDayNumber + difference
+
+      let years400, remaining400 =
+         dayNumber / daysIn400Years, dayNumber % daysIn400Years
+
+      let years40, remaining40 =
+         remaining400 / daysIn40Years, remaining400 % daysIn40Years
+
+      let years5, remaining5 =
+         remaining40 / daysIn5Years, remaining40 % daysIn5Years
+
+      let remainingYears, remainingDays = remaining5 / 364, remaining5 % 364
+
+      let year =
+         years400 * 400
+         + years40 * 40
+         + years5 * 5
+         + Math.Min(remainingYears, 4)
+
+      let dayOfYear =
+         if remainingYears = 5 then
+            364 + remainingDays // leap year
+         else
+            remainingDays
+
+      let season = if dayOfYear >= 364 then 4 else dayOfYear / 91 + 1
+      let dayInSeason =
+         if dayOfYear >= 364 then
+            91 + dayOfYear % 91
+         else
+            dayOfYear % 91
+      let week = dayInSeason / 7 + 1
+      let dayOfWeek = dayOfYear % 7 + 1
+      $"%d{year + 1} %s{seasons.[Math.Min(4, season) - 1]} %s{weeks.[week - 1]} %s{daysOfWeek.[dayOfWeek - 1]}"
---
+++ SilicanDate/test/SilicanDate.Tests/FunctionTest.fs
@@ -0,0 +1,23 @@
+namespace SilicanDate.Tests
+
+
+open Xunit
+open Amazon.Lambda.TestUtilities
+
+open SilicanDate
+
+
+module FunctionTest =
+    [<Fact>]
+    let ``Invoke Lambda Function``() =
+        // Invoke the lambda function and confirm the string was upper cased.
+        let lambdaFunction = Function()
+        let syncPoint = lambdaFunction.FunctionHandler "2018-01-01"
+
+        Assert.Equal("12018 Nevari Ateluna Boromika", syncPoint)
+        
+        let test2 = lambdaFunction.FunctionHandler "2018-01-07"
+        Assert.Equal("12018 Nevari Ateluna Temiranika", test2)
+
+    [<EntryPoint>]
+    let main _ = 0
